6d5e0796701c6089fc6a65fc4d0625d74d8fa317
Dan Baumfeld <thethirddan@gmail.com>
2015-08-05 14:56:41 -0700

    added authors

--- server/mocks/posts.js
+++ server/mocks/posts.js
@@ -7,11 +7,20 @@ module.exports = function(app) {
       'posts': [
         {
           id: 1,
-          title: 'Bananas'
+          title: 'Bananas',
+          author: 1
         },
         {
           id: 2,
-          title: 'Apples'
+          title: 'Apples',
+          author: 1
+        }
+      ],
+      'authors': [
+        {
+          id: 1,
+          name: 'Dan Baumfeld',
+          posts: [1,2]
         }
       ]
     });
