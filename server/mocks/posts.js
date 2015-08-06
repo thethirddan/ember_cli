ec91e15b07f7ab01726ad1a46501cf5e4fe45d59
Dan Baumfeld <thethirddan@gmail.com>
2015-08-05 17:11:53 -0700

    added post page

--- server/mocks/posts.js
+++ server/mocks/posts.js
@@ -1,28 +1,56 @@
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    if (this === null) {
+      throw new TypeError('Array.prototype.find called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return value;
+      }
+    }
+    return undefined;
+  };
+}
+var posts = [
+  {
+    id: 1,
+    title: 'Bananas',
+    author: 1,
+    body: 'sword swrod soweroefk wofdf'
+  },
+  {
+    id: 2,
+    title: 'Apples',
+    author: 1,
+    body: 'words words words words words'
+  }
+];
+
+var authors = [
+   {
+     id: 1,
+     name: 'Dan Baumfeld',
+     posts: [1,2]
+   }
+ ];
+
 module.exports = function(app) {
   var express = require('express');
   var postsRouter = express.Router();
 
   postsRouter.get('/', function(req, res) {
     res.send({
-      'posts': [
-        {
-          id: 1,
-          title: 'Bananas',
-          author: 1
-        },
-        {
-          id: 2,
-          title: 'Apples',
-          author: 1
-        }
-      ],
-      'authors': [
-        {
-          id: 1,
-          name: 'Dan Baumfeld',
-          posts: [1,2]
-        }
-      ]
+      'posts': posts,
+      'authors': authors
     });
   });
 
@@ -32,9 +60,10 @@ module.exports = function(app) {
 
   postsRouter.get('/:id', function(req, res) {
     res.send({
-      'posts': {
-        id: req.params.id
-      }
+      'posts': posts.find(function(post) {
+        return post.id == req.params.id;
+      }),
+      'authors': authors
     });
   });
 
